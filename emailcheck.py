83f5324ab3ebf0b3492e9b53271705170d3d2222
Nicholas Frischkorn <45154327+NickFrischkorn@users.noreply.github.com>
2021-03-17 12:27:49 -0400

    Update emailcheck.py

--- emailcheck.py
+++ emailcheck.py
@@ -6,11 +6,11 @@ key = "" # -- Insert HIBP APIv3 key here --
 email_file = "" # -- Insert path to file containing email list --
 
 master_output = []
-head = {"User-Agent": "HIBP-Email-Checker". "hibp-api-key": key}
+head = {"User-Agent": "HIBP-Email-Checker", "hibp-api-key": key}
 email_list = open(email_file)
 
 for email in email_list:
-    email = email.replace('\n', '')
+    email = email.replace('\n', '') # remove trailing carriage return
     time.sleep(1.7) # rate limiting for APIv3 is 1500 milliseconds, so 1700 to avoid triggering
     try:
         hibp_check = requests.get(url='https://haveibeenpwned.com/api/v3/breachedaccount/' + email, headers=head)
